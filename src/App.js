54bea6129b31614189ece202d797bb755e6b0271
suhail_khan28 <suhail.khan@walletdoc.com>
2021-07-16 23:40:36 +0530

    Route added on App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
+    <Router>
+      <Route path="/" exact>
+        <Dashboard></Dashboard>
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="error">
+        <Error />
+      </Route>
+    </Router>
   );
 }
 
